55afb9c54f27540874667862d489567538b1e311
Vladimir Tolmachev <vladimir.tolmachev@oneclickdev.com>
2020-12-04 21:17:36 +0300

    changes

---
+++ src/AudioBookPlayer.App/AudioBookPlayer.App.Android/Controllers/AndroidPlaybackController.cs
@@ -0,0 +1,12 @@
+﻿using AudioBookPlayer.App.Core.Controllers;
+using AudioBookPlayer.App.Droid.Controllers;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AndroidPlaybackController))]
+
+namespace AudioBookPlayer.App.Droid.Controllers
+{
+    internal sealed class AndroidPlaybackController : IPlaybackController
+    {
+    }
+}
