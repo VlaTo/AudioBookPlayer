ed80779c70b3cb487d447d12fbd38a6ca80f8bac
Vladimir Tolmachev <vladimir.tolmachev@oneclickdev.com>
2020-12-09 19:05:50 +0300

    changes

---
+++ src/AudioBookPlayer.App/AudioBookPlayer.App/Views/ChooseLibraryFolderPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using LibraProgramming.Xamarin.Popups.Platforms.Xamarin;
+using Xamarin.Forms.Xaml;
+
+namespace AudioBookPlayer.App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChooseLibraryFolderPopup : PopupContentPage
+    {
+        public ChooseLibraryFolderPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/LibraProgramming.Modules/LibraProgramming.Xamarin.Modules/LibraProgramming.Xamarin.Popups/Core/IPlatformPopup.cs
@@ -0,0 +1,17 @@
+﻿using LibraProgramming.Xamarin.Popups.Platforms.Xamarin;
+using System.Threading.Tasks;
+
+namespace LibraProgramming.Xamarin.Popups.Core
+{
+    public interface IPlatformPopup
+    {
+        bool IsInitialized
+        {
+            get;
+        }
+
+        Task AddAsync(PopupContentPage page);
+
+        Task RemoveAsync(PopupContentPage page);
+    }
+}
