33829b9b407272008114170ff7889161fad5d5aa
Vladimir Tolmachev <vladimir.tolmachev@oneclickdev.com>
2021-08-20 17:30:47 +0300

    splashscreen style changes; audio position tracking changes

--- src/AudioBookPlayer.App/AudioBookPlayer.App.Android/SplashActivity.cs
+++ src/AudioBookPlayer.App/AudioBookPlayer.App.Android/SplashActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.OS;
 using AndroidX.AppCompat.App;
 
 namespace AudioBookPlayer.App.Android
@@ -7,18 +8,13 @@ namespace AudioBookPlayer.App.Android
     [Activity(Label = "@string/app_name", Icon = "@mipmap/icon", Theme ="@style/MainSplash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : AppCompatActivity
     {
-        protected override void OnStart()
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnStart();
-
-        }
-
-        // Launches the startup task
-        protected override void OnResume()
-        {
-            base.OnResume();
-
+            base.OnCreate(savedInstanceState);
+            
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+            Finish();
         }
     }
 }
